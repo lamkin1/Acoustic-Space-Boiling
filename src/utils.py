f7cc24344a5927b691792d20eeb6d6a7eb84542a
AndrewJMart <amart89531@gmail.com>
2025-05-15 10:55:39 -0700

    Added NN

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.signal as signal
 from pathlib import Path
 import matplotlib.pyplot as plt
+from scipy.stats import binom
 
 
 def get_file(file):
@@ -22,6 +23,34 @@ def unpack_data(data):
 
 def clamp(value, lower=0.015, upper=0.1):
     return max(lower, min(value, upper))
+
+def get_diffs(x, margin=0.003, p_null=0.1, alpha=0.05):
+    x = np.sort(x)
+    range_x = x[-1] - x[0]
+    cutoff = range_x / 2
+
+    diffs = []
+    for i in range(len(x) - 1):
+        for j in range(i + 1, len(x)):
+            d = round(float(x[j] - x[i]), 4)  # round to get rid of floating point error
+            if d > cutoff:
+                break
+
+            anchor = x[i]
+            hits = 0
+            tries = 0
+            t = anchor
+            while t <= x[-1]:
+                if np.any(np.abs(x - t) <= margin):
+                    hits += 1
+                tries += 1
+                t += d
+
+            p_value = 1 - binom.cdf(hits - 1, tries, p_null) if hits < tries else 0.0
+            if p_value < alpha:
+                diffs.append(d)
+
+    return sorted(diffs)
 
 
 def get_peaks(acceleration0):
